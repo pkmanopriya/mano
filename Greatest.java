b2c49a569456f99894acba0580cbddbb61a7beb7
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-10-31 21:21:09 +0530

    Create Greatest.java

---
+++ Greatest.java
@@ -0,0 +1,13 @@
+package javaprogram
+public class Greatest {
+    public static void main(String[]args){
+      int a=55,b=80;
+      if(a>b){
+         System.out.printIn("a is greater than b");
+      }
+        else{
+          System.out.printIn("b is greater than a");
+        }
+    }
+}
+  
