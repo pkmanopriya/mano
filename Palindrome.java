2a0f96f7722ef3ada784a699274086751bcc41b4
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-10-31 21:36:54 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+package javaprogram;
+
+public class palindrome {
+   public static void main(String []args){
+     int r,sum=0,temp;
+     int n=454;
+
+     temp=n;
+     While (n>0){
+      r=n/10;
+     }
+     if(temp==sum)
+       System.out.printIn("palindrome number");
+
+      else{
+        System.out.printIn("not palindrome");
+      }
+   }
+}
