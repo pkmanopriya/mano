b46f852afdee6f41975cc98a796fdf6e92506669
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-10-31 20:54:32 +0530

    Fact.java

---
+++ Fact.java
@@ -0,0 +1,12 @@
+package java program;
+
+public class Fact{
+      public static void main(System []args);
+      int i,fact=1;
+      int number=5;
+       for (i=1;i<=number;i++){
+           fact=fact*i;
+}
+ System.out.printIn("Factorial of"+ number+:"+fact);
+} 
+}
