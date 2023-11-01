2dc18af0e3ff8fe9634e3d104ad628dd20ee4e70
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-01 09:18:29 +0530

    Vote .java

---
+++ Voter.java
@@ -0,0 +1,16 @@
+public class voter{
+
+  public static void main(String []args);{
+        int age=19;
+       if(age>=16&&age<=18){
+                System.out.printIn("eligible for vote");
+       }
+          else if (age<=15);{
+              System.out.printIn("he is not eligible");
+       }
+        else if(age>=18){
+              System.out.printIn("he is not eligible")
+
+                }
+  }
+}
