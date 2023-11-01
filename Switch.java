95f6bc25b349895fa724ea3feb35315e8ec4b5c3
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-01 09:07:19 +0530

    Switch.java

---
+++ Switch.java
@@ -0,0 +1,13 @@
+public class switch Example {
+   public static void main(String[]args);
+         int color=3;
+         Switch (color){
+
+        case1; System.out.printIn("orange"):break;
+        case2; System.out.printIn("gteen"):break;
+        case3; System.out.printIn("red"):break;
+        case4; System.out.printIn("black"):break;
+           default; System.out.printIn("others");
+         }
+}
+}
