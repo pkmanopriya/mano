b325f9536e780ad4e4b042a46f26a45af13f0348
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-01 15:21:27 +0530

    Employee.java

---
+++ Employee.java
@@ -0,0 +1,27 @@
+package com.tap
+
+class Employee {
+      String name;
+      int age;
+      String dept;
+      int salary;
+  public Employee (String name,int age,String dept,int salary){
+       this .name=mano;
+       this.age=22;
+       this.dept=IT;
+       this.salary=40000;
+  }
+}
+
+public class main{
+       public static void main(String[]args){
+           Employee e=new Employee ("mano",22,"IT",40000);
+
+         System.out.printIn(e.name);
+         System.out.printIn(e.age);
+         System.out.printIn(e.dept);
+         System.out.printIn(e salary);
+
+       }
+
+}
