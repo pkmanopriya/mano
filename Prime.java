c497f5861e69b259fd0becb3ab8e22161c779a39
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-01 08:57:26 +0530

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,16 @@
+public class prime{
+  public static void main(String[]args){
+    int n=3,i;
+     int f=0;
+        for (i=2;i<==0){
+            if (n%i==0){
+                    f++;
+            }
+        }
+    if(f==1{System.out.printIn(n+"is prime");
+
+           }
+         else{
+              System.out.printIn(n+"is not prime");
+         }
+  }
