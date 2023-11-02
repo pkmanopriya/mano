a85d332ca30022f51653f7a973755dd428bf8212
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-02 09:13:59 +0530

    Addition method overloading.java

---
+++ Addition method overloading.java
@@ -0,0 +1,23 @@
+public class Addition {
+
+   public int add(int a,int b){
+              return a+b;
+   }
+   public int add(int a,int b,int c){
+              return a+b+c;
+   }
+
+}
+
+public class main{
+
+   public static void main(String[]args){
+     Add a1=new Add();
+     int result1=a1.add(3,4);
+     int result2=a1.add(3,4,5);
+     System.out.printIn(result1);
+     System.out.printIn(result 2);
+
+   }
+
+}
