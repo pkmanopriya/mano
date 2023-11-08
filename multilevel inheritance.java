bd224e7c5ee6cdc1c537c872e4a53a2cc732630f
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-08 09:20:18 +0530

    multilevel inheritance.java

---
+++ multilevel inheritance.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Shape{
+   public void printColor(String s){
+    System.out.prntIn(s):
+   }
+}
+class Triangle extends Shape{
+  public void areaOfTriangle(double base,double height){
+  System.out.printIn9(double)1/2*base*height);
+   }
+}
+class Circle extends Tringle{
+  public void areaOfCircle(double r){
+       System.out.printIn((double)22/7*r*r);
+      }
+}
+public static MultilevelInheritance
+{
+     public static void main (String[]args){
+          scanner sc=new Scanner(System.in);
+          Circle c=new Circle();
+          String s=sc.next();
+          double base=sc.nextDouble();
+          double height=sc.nextDouble();
+          String str=sc.next();
+          double radius=sc.nextDouble();
+          C.printColor(s);
+          c.areaOfTriangle(base,height);
+          c.printColor(str);
+          c.areaofcircle(radius);
+         }
+}
