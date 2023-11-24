83982a2fdbdc9f809b7a5e132dcf8a96810ae8ad
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-24 08:15:23 +0530

    Add files via upload

---
+++ encapsulation/Employee.java
@@ -0,0 +1,15 @@
+package com.encapsulation;
+public class Employee {
+	   private int id;
+	   String name;
+	   protected String deptName;
+	   String location;
+	   public int getId() {
+		      return id;
+	   }
+	   public void setId(int n) {
+		      id=n;
+	   }
+	   
+}
+	  
---
+++ encapsulation/Main.java
@@ -0,0 +1,19 @@
+package com.encapsulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		    Employee d=new Employee();
+		    d.name="Mano";
+		    d.deptName="Developer";
+		    d.location="Chennai";
+		    d.setId(5432);
+		    System.out.println(d.name);
+		    System.out.println(d.deptName);
+		    System.out.println(d.location);
+		    System.out.println(d.getId());
+		    
+	}
+	
+}
+		    
