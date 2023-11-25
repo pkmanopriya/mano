4b708ffc70a485c57ee71ce6863f3082f1cdba58
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-25 14:08:29 +0530

    Add files via upload

---
+++ arrayAsObject/Employee.java
@@ -0,0 +1,13 @@
+package com.arrayAsObject;
+
+public class Employee {
+	   int id;
+	   String name;
+	   String location;
+	   Employee(int id,String name,String location){
+		       this.id=id;
+		       this.name=name;
+		       this.location=location;
+	   }
+}
+	  
---
+++ arrayAsObject/Main.java
@@ -0,0 +1,19 @@
+package com.arrayAsObject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		    Employee employee[]=new Employee[4];
+		    employee[0]=new Employee(101,"abc","chennai");
+		    employee[1]=new Employee(102,"efg","banglore");
+		    employee[2]=new Employee(103,"ijk","coimbatore");
+		    employee[3]=new Employee(104,"xyz","kerala");
+	}
+	
+}  
+
+
+	
+
+
+		    		
