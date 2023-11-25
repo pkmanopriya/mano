4a08376f8feaae412481f9aa8425434b65e5e987
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-25 14:17:19 +0530

    Add files via upload

---
+++ multiDimensionalArray/src/com/multiDimensionalArray/MultiDimensionalArray.java
@@ -0,0 +1,14 @@
+package com.multiDimensionalArray;
+
+public class MultiDimensionalArray {
+       public static void main(String[] args) {
+    	     int arr1[][]= {{3,4,5},{6,7,8},{9,10,11}};
+    	     for (int a[]:arr1) {
+    	    	    for (int j:a) {
+    	    	    	System.out.println(j+" ");
+    	    	    }
+    	    	        System.out.println();
+    	     }
+       }
+}
+
