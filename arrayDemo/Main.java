47037a98af2d6c891e7e8d1df33b9b1ef1c5e843
pkmanopriya <149451815+pkmanopriya@users.noreply.github.com>
2023-11-25 14:09:37 +0530

    Add files via upload

---
+++ arrayDemo/Main.java
@@ -0,0 +1,16 @@
+package com.arrayDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		   int a[]=new int[3];
+		   a[0]=1;
+		   a[1]=2;
+		   a[2]=3;
+		   for(int i=0;i<a.length;i++) {
+			   System.out.println("Element at Index:"+(i)+"="+a[i]);
+		   }
+	}
+}
+		   
+		   
